1602c6b4f19b983457e08a2c500d05728f0eb04c
Tyler Long <tyler4long@gmail.com>
2023-05-04 10:38:02 -0700

    Update numbers

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,14 @@
 import {useProxy} from '@tylerlong/use-proxy';
 
 export class Store {
-  totalCost = 1000000;
-  outOfPocket = 100000;
-  investmentInterest = 4.15;
-  mortgageInterest = 2.25;
-  propertyTax = 1000;
-  hoa = 500;
-  insurance = 150;
-  rent = 3300;
+  totalCost = 1500000;
+  outOfPocket = 500000;
+  investmentInterest = 4.65;
+  mortgageInterest = 8.25;
+  propertyTax = 1500;
+  hoa = 700;
+  insurance = 200;
+  rent = 5000;
 }
 
 const store = useProxy(new Store());
